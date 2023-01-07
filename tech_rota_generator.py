66e5cd73ec55b01b35e55c1bb13a5de72364f679
Lydiaade <lydiaadejumo@outlook.com>
2023-01-07 23:52:48 +0000

    Update instruction readability

--- tech_rota_generator.py
+++ tech_rota_generator.py
@@ -48,12 +48,11 @@ if __name__ == "__main__":
     elif len(sys.argv) == 4:
         generate_rota(start_month_year=sys.argv[1], end_month_year=sys.argv[2], team_members_csv=sys.argv[3])
     else:
-        print(
-            "To use this script you would need to input the following:")
+        print("\nTo use this script you would need to input the following:")
         print("1: Start month and year")
         print("2: End month and year")
         print("3: Team members csv file, if not the default will be used.")
-        print("Note: If used it must be placed in the team members folder")
-        print(f"python {os.path.basename(__file__)} start_month_and_year end_month_and_year team_members_csv_file_name")
+        print("Note: If used it must be placed in the team members folder\n")
         print("Example:")
+        print(f"python {os.path.basename(__file__)} start_month_and_year end_month_and_year team_members_csv_file_name")
         print(f"python {os.path.basename(__file__)} 01/2023 07/2023 team_members.csv")
